3939ca96cad3662cc2577ccbcf7d13e0b0ff4c3a
pellet0000 <clement.pellet-mary@unibas.ch>
2024-08-09 11:20:49 +0200

    toto

--- Data analysis/CrSBr_2024_08_07.py
+++ Data analysis/CrSBr_2024_08_07.py
@@ -51,6 +51,18 @@ def ESRmap_0808():
 
 # ESRmap_0808()
 
+def ESRmap_0809():
+    fname=r'K:\Experiments\Experiment_203_LT\NVAFM_203_LT\appdesigner_data\RF_imaging_temp\RFscan_20240809_001.mat'
+    scanESR=mat.NVAFM_RFscan_matFile(fname)
+    a=scanESR.ESRMap(refit=False)
+    a=a[:,:]
+    a=(a-a[0,0])*1000/28
+    xAxis=scanESR.xrange
+    yAxis=scanESR.yrange
+    plot_map(a, vAxis=xAxis, hAxis=yAxis, color='bwr' ,colorBarLabel='B (mT)' , invertH=True,invertV=True, centerColorBar=0)
+
+ESRmap_0809()
+
 def ESRmap_0726():
     fname=r'K:\Experiments\Experiment_203_LT\NVAFM_203_LT\appdesigner_data\RF_imaging_temp\RFscan_20240726_001.mat'
     scanESR=mat.NVAFM_RFscan_matFile(fname)
@@ -216,6 +228,25 @@ def linescan_avg_0806_08(show=True):
 
 # linescan_avg_0806_08()
 
+def linescan_0806_08_firstLine(show=True):
+    fname=r'K:\Experiments\Experiment_203_LT\NVAFM_203_LT\appdesigner_data\Imaging\2024\scan_20240806_008.mat'
+    scan=mat.NVAFM_scan_matFile(fname)
+    scan.findRange()
+    a=scan.dataDic['RFFreq forward']
+    a=a-a[0,0]
+    a*=1000
+    line=a[0,:]
+    xAxis=np.linspace(scan.ymin,scan.ymax,len(line))
+    xAxis=(xAxis-min(xAxis))*2000
+    if show:
+        plt.plot(xAxis,line)
+        plt.xlabel('Y (nm)')
+        plt.ylabel('Frequency (MHz)')
+        plt.show()
+    return xAxis,line
+
+# linescan_0806_08_firstLine()
+
 def linescan_0806_10(show=True):
     fname=r'K:\Experiments\Experiment_203_LT\NVAFM_203_LT\appdesigner_data\Imaging\2024\scan_20240806_010.mat'
     scan=mat.NVAFM_scan_matFile(fname)
@@ -271,4 +302,24 @@ def linescan_0806_12(show=True):
         plt.show()
     return xAxis,line
 
-linescan_0806_12()
+# linescan_0806_12()
+
+def plotAllLines():
+    xAxis,line=linescan_avg_0806_05(show=False)
+    plt.plot(xAxis,line,label='100 G')
+    xAxis,line=linescan_avg_0806_06(show=False)
+    plt.plot(xAxis,line+10,label='700 G')
+    xAxis,line=linescan_0806_08_firstLine(show=False)
+    plt.plot(xAxis,line+20,label='1300 G')
+    xAxis,line=linescan_0806_10(show=False)
+    plt.plot(xAxis,line+30,label='1500 G')
+    xAxis,line=linescan_0806_11(show=False)
+    plt.plot(xAxis,line+40,label='1700 G')
+    xAxis,line=linescan_0806_12(show=False)
+    plt.plot(xAxis,line+50,label='1900 G')
+    plt.xlabel('Y (nm)')
+    plt.ylabel('Frequency (MHz)')
+    plt.legend()
+    plt.show()
+
+# plotAllLines()
